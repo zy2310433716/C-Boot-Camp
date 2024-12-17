4c0104d324d362dd93cff6ffcbf1b921dedfa208
zy2310433716 <2310433716@qq.com>
2024-12-17 20:41:38 +0800

    update

---
+++ homework/2024-12-17/你是天才吗.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+
+
+
+int main()
+{
+    int i = 0;
+    scanf("%d",&i);
+    if(i >= 140)
+    {
+        printf("Genius");
+    }
+    return 0;
+} 
---
+++ homework/2024-12-17/比较两个数大小.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main()
+{
+    int a = 0;
+    int b = 0;
+    while(scanf("%d %d",&a,&b)!=EOF)
+    if(a == b)
+    {
+        printf("%d=%d\n",a,b);
+    }
+    else if(a > b)
+    {
+        printf("%d>%d\n",a,b);
+    }
+    else
+    {
+        printf("%d<%d\n",a,b);
+    }
+       
+    return 0;
+}
